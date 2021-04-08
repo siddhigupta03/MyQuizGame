2611012b64d774e01a1eb288103663245056ad2d
siddhigupta03 <80528782+siddhigupta03@users.noreply.github.com>
2021-04-08 18:11:58 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,63 @@
+class Quiz {
+  constructor(){
+    this.title = createElement('h1');
+    this.greetings = createElement('h4');
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",(data)=>{
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question();
+      question.display();
+    }
+  }
+
+  play() {
+   question.hide();
+   background("yellow");
+   this.title.html("Result of Quiz");
+   this.title.position(350,10);
+   Contestant.getPlayerInfo();
+
+    if(allContestants !== undefined) {
+      fill("blue");
+      textSize(20);
+      text("*Note, the contastant who answered correctly are highlighted in green.",130,230);
+          for (var con in allContestants) {
+            if(con === "contestant"+ contestant.index) {
+              var corAns = "2";
+              if(corAns === allContestants[con].answer){
+                fill("green")
+                text(allContestants[con].name+": "+allContestants[con].answer,260,300);
+          }
+          else {
+              fill ("red");
+              text(allContestants[con].name+": "+allContestants[con].answer,260,300);
+            }
+            if(contestant.index!==null) {
+              contestant.update(); 
+            }
+          }
+        }
+      }
+    }
+  }
